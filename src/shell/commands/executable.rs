7514eba7d5860a51a9112c6f93416d8edaea4bf4
Martin Olsson <martin@minimum.se>
2024-11-13 09:51:34 +0100

    feat: Kill child processes when task shell receives ctrl-c

--- src/shell/commands/executable.rs
+++ src/shell/commands/executable.rs
@@ -67,6 +67,18 @@ impl ShellCommand for ExecutableCommand {
             ExecuteResult::Continue(1, Vec::new(), Vec::new())
           }
         },
+        _ = tokio::signal::ctrl_c() => {
+          println!("");
+          println!("ctrl-c detected: {}", command_name.display());
+          println!("");
+          if let Ok(None) = child.try_wait() {
+            println!("");
+            println!("killing: {}", command_name.display());
+            println!("");
+            let _ = child.kill().await;
+          }
+          ExecuteResult::for_cancellation()
+        }
         _ = context.state.token().cancelled() => {
           let _ = child.kill().await;
           ExecuteResult::for_cancellation()
